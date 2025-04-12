e60bb097a0ffbf184b80617fdd05c43883a34438
Adrian Osorio Blanchard <osorioadrian04@gmail.com>
2025-04-12 16:28:14 -0400

    mod files to aiQuery and db merge

--- packages/AI/aiQuery.ts
+++ packages/AI/aiQuery.ts
@@ -12,7 +12,7 @@ const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY);
 export async function queryAI(prompt: string): Promise<string> {
   try {
     const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash-lite' });
-    const result = await model.generateContent("testing hi");
+    const result = await model.generateContent("Give the HTS code for this product: Apple 2025 MacBook Air 13-inch Laptop with M4 chip: Built for Apple Intelligence, 13.6-inch Liquid Retina Display, 16GB Unified Memory, 256GB SSD Storage, 12MP Center Stage Camera, Touch ID; Sky Blue , No YAP ONLY THE CODE ");
     const response = await result.response;
     return response.text();
   } catch (error) {
