a1fb3adbc038dd682e639a0566c109f03399bda4
Elizabeth Pretto-Sotelo ⊹.✮₊⋆ <elizabethprettosotelo@gmail.com>
2025-04-12 22:17:50 -0400

    guide progress

--- apps/website/src/components/navbar.tsx
+++ apps/website/src/components/navbar.tsx
@@ -6,9 +6,9 @@ import { XMarkIcon, Bars3Icon } from "@heroicons/react/24/solid";
 import { Button } from "@/components/ui/button";
 
 const routes: { title: string; href: string }[] = [
-  { title: "GUIDE", href: "/guide" },
-  { title: "WATCH", href: "/watch" },
-  { title: "DOWNLOAD", href: "/download"}
+  { title: "Guide", href: "/guide" },
+  { title: "Watch", href: "/watch" },
+  { title: "Download", href: "/download"}
 ];
 
 const Navbar: React.FC = () => {
@@ -23,7 +23,7 @@ const Navbar: React.FC = () => {
       <div className="flex w-full justify-between m-6">
         <div className="flex justify-start items-center space-x-4">
           <Link href={"/"} className="shrink-0">
-            <Image className="object-contain" src="/" alt="My Icon" width={150} height={150} />
+            <Image className="object-contain" src="/tarrifixblacklogo.svg" alt="My Icon" width={150} height={150} />
           </Link>
         </div>
 
@@ -32,7 +32,7 @@ const Navbar: React.FC = () => {
             <Link
               key={index}
               href={route.href}
-              className={route.title === "DOWNLOAD" ? "font-bold transition items-center inline-flex lg:px-5 md:px-3 sm:px-1.5 lg:text-base md:text-sm sm:text-xs text-white bg-[#8D6B94] h-fit py-2 my-18 w-fit hover:bg-[#d096dc]" : "font-bold transition items-center inline-flex lg:px-5 md:px-3 sm:px-1.5 lg:text-base md:text-sm sm:text-xs hover:text-[#8D6B94] text-black"}
+              className={route.title === "Download" ? "font-bold transition items-center inline-flex lg:px-5 md:px-3 sm:px-1.5 lg:text-base md:text-sm sm:text-xs text-white bg-[#8D6B94] h-fit py-2 my-18 w-fit hover:bg-[#d096dc]" : "font-bold transition items-center inline-flex lg:px-5 md:px-3 sm:px-1.5 lg:text-base md:text-sm sm:text-xs hover:text-[#8D6B94] text-black"}
             >
               {route.title}
             </Link>
@@ -64,7 +64,7 @@ const MobileMenu: React.FC<{ toggleMenu: () => void }> = ({ toggleMenu }) => {
             key={index}
             href={route.href}
             onClick={toggleMenu}
-            className={route.title === "DOWNLOAD" ? "hover:bg-[#d096dc] font-bold text-white justify-center inline-flex align-middle text-center h-10 w-full items-center text-sm transition-colors bg-[#8D6B94] p-5" : "hover:text-[#679436] font-bold text-white inline-flex h-10 w-full items-center text-sm transition-colors"}
+            className={route.title === "Download" ? "hover:bg-[#d096dc] font-bold text-white justify-center inline-flex align-middle text-center h-10 w-full items-center text-sm transition-colors bg-[#8D6B94] p-5" : "hover:text-[#679436] font-bold text-white inline-flex h-10 w-full items-center text-sm transition-colors"}
             
             
           >
